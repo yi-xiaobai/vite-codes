ebc2371b765461f55789f6e8ae9d82fe82d06dfc
yi.luo <lyyl6270@163.com>
2024-06-20 17:13:09 +0800

    perf:dist 忽略

--- vite-typescript/dist/assets/main-9fxmvtDH.js
+++
@@ -1,1 +0,0 @@
-import"./product-YFxwSTwT.js";import{l as r}from"./vendor-TVC-EY0t.js";const l=[];r.forEach(l,o=>{console.log("==>Get elm",o),console.log("==>Get 1111",o)});
--- vite-typescript/vite.config.ts
+++ vite-typescript/vite.config.ts
@@ -19,12 +19,16 @@ export default defineConfig({
       output: {
         // 针对于 node_modules中的包 本身无法改变 随意可以单独拎出来打包生成chunk
         // 减少文件体积 优化项目
-        manualChunks: (id) => {
-          console.log("==>Get id", id);
-          if (id.includes("node_modules")) {
-            return "vendor";
-          }
+        // manualChunks: (id) => {
+        //   console.log("==>Get id", id);
+        //   if (id.includes("node_modules")) {
+        //     return "vendor";
+        //   }
+        // },
+        manualChunks: {
+          lodash: ["lodash"],
         },
+        //* 上述例子中，即使你只是使用 import get from 'lodash/get' 形式引入，Rollup 也会将 lodash 的所有模块放到一个自定义 chunk 中
       },
     },
   },
